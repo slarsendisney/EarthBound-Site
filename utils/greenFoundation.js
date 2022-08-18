6353bf182fa8731bf5c565801da5a96961cac811
Sam Larsen-Disney <sam@amplitude.com>
2022-08-18 19:50:13 +0100

    fixed green web foundation urls

--- utils/greenFoundation.js
+++ utils/greenFoundation.js
@@ -1,6 +1,8 @@
 const greenFoundation = async (url) => {
+  const urlNoProtocol = url.replace(/(^\w+:|^)\/\//, '');
+  const urlNoPath = urlNoProtocol.split('/')[0];
   const response = await fetch(
-    `https://admin.thegreenwebfoundation.org/api/v3/greencheck/${url}`
+    `https://admin.thegreenwebfoundation.org/api/v3/greencheck/${urlNoPath}`
   );
   const data = await response.json();
   return data;
